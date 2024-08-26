8c5a36ece2c5fd58fa7acfced3b353cbd649d28a
John Michael Wu <johnmichaelwu@gmail.com>
2024-08-26 02:05:32 -0400

    11.8 all tests pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
@@ -40,24 +40,26 @@ const App = () => {
   }
 
   return (
-    <Routes>
-      <Route
-        exact
-        path="/"
-        element={<PokemonList pokemonList={pokemonList} />}
-      />
-      <Route
-        exact
-        path="/pokemon/:name"
-        element={
-          <PokemonPage
-            pokemonList={pokemonList}
-            previous={previous}
-            next={next}
-          />
-        }
-      />
-    </Routes>
+    <div>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<PokemonList pokemonList={pokemonList} />}
+        />
+        <Route
+          exact
+          path="/pokemon/:name"
+          element={
+            <PokemonPage
+              pokemonList={pokemonList}
+              previous={previous}
+              next={next}
+            />
+          }
+        />
+      </Routes>
+    </div>
   )
 }
 
